cff01905fcbaa5ccc2cb16c2223fac1810324676
whysman <whysman@whystech.com>
2020-04-28 01:47:51 -0400

    Added websockets to replace endpoints

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module trivia-server
 
 go 1.14
 
-require github.com/docker/docker v1.13.1
+require (
+	github.com/docker/docker v1.13.1
+	github.com/gorilla/websocket v1.4.2
+)
--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/docker/docker/pkg/namesgenerator"
+	"github.com/gorilla/websocket"
 )
 
 type gameInfo struct {
@@ -23,18 +24,97 @@ type userJoin struct {
 	GameID string `json:"gameid"`
 }
 
+type message struct {
+	Command string `json:"command"`
+	Data    string `json:"data"`
+}
+
 var activeGames = make(map[string]gameInfo)
+var upgrader = websocket.Upgrader{}
 
 func main() {
-	http.HandleFunc("/createGame", createGame)
-	http.HandleFunc("/listGames", listGames)
-	http.HandleFunc("/joinGame", joinGame)
+	//http.HandleFunc("/createGame", createGame)
+	//http.HandleFunc("/listGames", listGames)
+	//http.HandleFunc("/joinGame", joinGame)
+	http.HandleFunc("/wscomm", wsComm)
 	err := http.ListenAndServe(":4000", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
+func wsComm(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		_, msg, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", msg)
+		m := message{}
+		json.Unmarshal(msg, &m)
+		processMessage(m)
+		err = c.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func getKeys(m map[string]gameInfo) (keys []string) {
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func processMessage(msg message) []byte {
+	var response []byte
+	if msg.Command == "createGame" {
+		response = createGame()
+	} else if msg.Command == "listGames" {
+		response = listGames()
+	} else if msg.Command == "joinGame" {
+		response = joinGame(msg.Data)
+	}
+	return response
+}
+
+func createGame() []byte {
+	name := namesgenerator.GetRandomName(0)
+	activeGames[name] = gameInfo{gameID: name}
+	jsonName, _ := json.Marshal(name)
+	return jsonName
+}
+
+func listGames() []byte {
+	jsonKeys, _ := json.Marshal(getKeys(activeGames))
+	return jsonKeys
+}
+
+func joinGame(data string) []byte {
+	b := []byte(data)
+	u := userJoin{}
+	json.Unmarshal(b, &u)
+	if _, ok := activeGames[string(u.GameID)]; ok {
+		ud := activeGames[string(u.GameID)].userData
+		ud = append(ud, userEntry{name: u.Name})
+		activeGames[string(u.GameID)] = gameInfo{gameID: string(u.GameID), userData: ud}
+		fmt.Println(activeGames[string(u.GameID)].userData)
+		return []byte("Joined game: " + data)
+	}
+	return []byte("Failed to join game: " + data)
+}
+
+/*
 func createGame(w http.ResponseWriter, r *http.Request) {
 	name := namesgenerator.GetRandomName(0)
 	activeGames[name] = gameInfo{gameID: name}
@@ -66,10 +146,4 @@ func joinGame(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	}
 }
-
-func getKeys(m map[string]gameInfo) (keys []string) {
-	for k := range m {
-		keys = append(keys, k)
-	}
-	return keys
-}
+*/
